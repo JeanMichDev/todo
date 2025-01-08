0b1850d916ec1c7197ea9fdda2ec184ca2df8e39
Theo Rutka <rutka.theo@gmail.com>
2025-01-08 11:41:00 +0100

    logique sur button add en fonction de session

--- auth.ts
+++ auth.ts
@@ -6,12 +6,12 @@ import Google from "next-auth/providers/google";
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   providers: [Google],
-  callbacks: {
-    session({ session, user }) {
-      if (!session.user) return session;
-      session.user.id = user.id;
-      session.user.image = user.image;
-      return session;
-    },
-  },
+  // // callbacks: {
+  // //   session({ session, user }) {
+  // //     if (!session.user) return session;
+  // //     session.user.id = user.id;
+  // //     session.user.image = user.image;
+  // //     return session;
+  // //   },
+  // },
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,62 +4,18 @@ import { TodoItem } from "./TodoItem";
 import { getAllActiveTodos, Todo } from "./todos.query";
 
 import { getAuthSession, getRequiredAuthSession } from "@/lib/authentication";
+import TodoForm from "./TodoForm";
 
 export default async function Home() {
   const session = await getAuthSession();
 
-  const user = session?.user;
-  console.log(user);
-
-  const todos = await getAllActiveTodos(user?.id);
+  const todos = await getAllActiveTodos(session?.user?.id);
 
   return (
     <main className="m-auto size-full max-w-3xl border bg-indigo-100 2xl:max-w-7xl">
       <section className="flex flex-col items-center justify-center gap-4 p-5">
         <h1 className="text-center text-xl font-semibold">Todo List</h1>
-        <form
-          className="grid w-full grid-cols-6 gap-2 border border-black"
-          action={async (formData: FormData) => {
-            "use server";
-            await createTodo(formData, user?.id);
-          }}
-        >
-          <InputDiv className="col-start-1 col-end-4">
-            <label htmlFor="title">Title</label>
-            <input
-              type="text"
-              name="title"
-              id="title" // Pour que label fonctionne bien il faut le lier à un id
-              placeholder="Name your task"
-            />
-          </InputDiv>
-          <InputDiv className="col-start-6">
-            <label htmlFor="deadline">Deadline</label>
-            <input type="date" name="deadline" id="deadline" />
-          </InputDiv>
-          <InputDiv className="col-start-1 col-end-5">
-            <label htmlFor="content">Content</label>
-            <textarea
-              name="content"
-              id="content"
-              placeholder="Describe your task"
-            />
-          </InputDiv>
-          <InputDiv className="col-start-6">
-            <label htmlFor="priority">Priority</label>
-            <select name="priority" id="priority" defaultValue="low">
-              <option value="low">Low</option>
-              <option value="medium">Medium</option>
-              <option value="high">High</option>
-            </select>
-          </InputDiv>
-          <button
-            type="submit"
-            className="col-start-3 col-end-5 rounded-md border bg-indigo-300 py-1 hover:bg-indigo-500"
-          >
-            Add
-          </button>
-        </form>
+        <TodoForm userId={session?.user?.id} />
         <section className="flex w-full flex-col items-start justify-center gap-4">
           {todos.map((todo: Todo) =>
             todo.isDone ? null : <TodoItem key={todo.id} todo={todo} />
